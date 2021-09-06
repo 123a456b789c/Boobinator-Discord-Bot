ee15cae8c6d574fe47c9ddb1aaaa72908733dcb3
123a456b789c <50078004+123a456b789c@users.noreply.github.com>
2021-09-06 19:57:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('TESZTOLDAL \n BOOBINATOR 2 - Discord bot \n Meghívható: https://discord.com/api/oauth2/authorize?client_id=863072664863768606&permissions=8&scope=bot'));
+app.get('/', (req, res) => res.send(`TESZTOLDAL \n ${name} - Discord bot \n Meghívható: https://discord.com/api/oauth2/authorize?client_id=863072664863768606&permissions=8&scope=bot`));
 app.listen(port, () => console.log(`A program a következő portszámot kapta:${port}`)); 
 
 //BOT KÓD KEZDÉSE
-
+var name = 'Boobinator'
 //Külső függvénykönyvtárak
 //npm i sqlite3
 //npm i sequelize
-var leveling = require('discord-leveling')
+const request = require('request');
+const billboardTop100 = require("billboard-top-100")
+const time = require('unix-timestamp-converter');
+const leveling = require('discord-leveling')
 const status = require('minecraft-server-status');
 const parser = require('rss-url-parser')
 const rf = require('reddit-image-fetcher')
@@ -19,7 +22,10 @@ const weather = require('weather-api-data');
 const Discord = require('discord.js');
 const disbut = require('discord-buttons');
 const nevnap = require ('nevnap');
+const Chart = require('chart.js')
+const ChartJsImage = require('chartjs-to-image');
 const DiscordPages = require("discord-pages");
+const currency = require("node-currency")
 const client = new Discord.Client();
 disbut(client);
 
@@ -28,6 +34,8 @@ client.on('ready', () => {
   console.log("Connected as " + client.user.tag);
   client.user.setActivity(".help", {type: "PLAYING"});
 });
+
+//Parancsok
 
 client.on('message', msg => {
   if (msg.content === '.minecraft') {
@@ -43,7 +51,7 @@ client.on('message', msg => {
   const embed = new Discord.MessageEmbed()
   .setColor('#2bff00')
   .setTitle(`:pick: *Hypixel* Státusz`)
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField(':video_game: Online:', emote)
   .addField(':restroom: Játékosok:', `${response.players.now}db / ${response.players.max}db`)
   .setImage('https://cdn.glitch.com/e58f9f02-0cf8-4a4a-b0a2-17c803c337b2%2Fpng-clipart-minecraft-grass-illustration-minecraft-pocket-edition-minecraft-story-mode-the-technomancer-xbox-360-ico-minecraft-miscellaneous-grass.png?v=1626368827244')
@@ -59,7 +67,7 @@ client.on('message', msg => {
   const error3 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Adatok lekérése sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Nincs megadva szerver.`)
 
   leveling.AddXp(msg.member.user.id, -1)
@@ -80,7 +88,7 @@ client.on('message', msg => {
   const embed = new Discord.MessageEmbed()
   .setColor('#2bff00')
   .setTitle(`:pick: *${url}* Státusz`)
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField(':video_game: Online:', emote)
   .addField(':restroom: Játékosok:', `${response.players.now}db / ${response.players.max}db`)
   .setImage('https://cdn.glitch.com/e58f9f02-0cf8-4a4a-b0a2-17c803c337b2%2Fpng-clipart-minecraft-grass-illustration-minecraft-pocket-edition-minecraft-story-mode-the-technomancer-xbox-360-ico-minecraft-miscellaneous-grass.png?v=1626368827244')
@@ -115,19 +123,19 @@ client.on('message', msg => {
   const error1 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Tag eltávolítása sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Nem jogosult a parancs használatához`)
 
   const error2 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Tag eltávolítása sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Felhasználó nem kirúgható.`)
 
   const error3 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Tag eltávolítása sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Nincs kijelölve tag, vagy nem megfelelő kijelölés. (Használja a @említés formátumot!`)
 
   let member = msg.mentions.members.first();
@@ -138,7 +146,7 @@ client.on('message', msg => {
   const embed = new Discord.MessageEmbed()
   .setColor('#2bff00')
   .setTitle("Siker - Tag eltávolítva")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .setImage('https://cdn.glitch.com/e58f9f02-0cf8-4a4a-b0a2-17c803c337b2%2F523-5232109_check-mark-computer-icons-clip-art-green-checkmark.png?v=1626359493911')
   msg.reply(embed)
 
@@ -155,19 +163,19 @@ client.on('message', msg => {
   const error1 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Törlés sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Nem jogosult a parancs használatához`)
 
 const error3 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Törlés sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Üzenetek száma mező nem értelmezhető vagy üres.`)
 
 const error2 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Törlés sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Üzenetek száma tatományon kívül (0 < szám > 100)`)  
 
 
@@ -190,7 +198,7 @@ let button = new disbut.MessageButton()
 const  ok = new Discord.MessageEmbed()
   .setColor('#ff0000')
   .setTitle("Nyomd meg a törlés megerősítéséhez!")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
 
 msg.channel.send(ok, button);
 
@@ -222,19 +230,19 @@ client.on('message', msg => {
   const error1 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Tag kitiltása sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Nem jogosult a parancs használatához`)
 
   const error2 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Tag kitiltása sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Felhasználó nem kirúgható.`)
 
   const error3 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Tag kitiltása sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Nincs kijelölve tag, vagy nem megfelelő kijelölés. (Használja a @említés formátumot!`)
 
 if (!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send(error1)
@@ -246,7 +254,7 @@ member.ban({reason: banReason})
   const embed = new Discord.MessageEmbed()
   .setColor('#2bff00')
   .setTitle("Siker - Tag eltávolítva")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .setImage('https://cdn.glitch.com/e58f9f02-0cf8-4a4a-b0a2-17c803c337b2%2F523-5232109_check-mark-computer-icons-clip-art-green-checkmark.png?v=1626359493911')
   msg.reply(embed)
 
@@ -272,7 +280,7 @@ for (var i = 0; i < szl.length; i++) {
   .setTitle('Üzenet Törölve')
   .setColor('#e60400')
   .setDescription("Szerver-moderáció")
-  .setAuthor(msg.author)
+  .setAuthor(`Vétkes: ${msg.author.username}`)
   .addField('Törlés oka', 'Üzenet tartalma szerepel a tiltólistán.')
   .addField('Üzenet', `||${msg.content}||`)
   .setImage('https://cdn.glitch.com/e58f9f02-0cf8-4a4a-b0a2-17c803c337b2%2F6053-3100-allj-elsobbsegadas-kotelezo-stop-tabla.png?v=1626338794156')
@@ -288,7 +296,7 @@ client.on('message', msg => {
 const error3 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - Időjárás betöltése sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Nincs kijelölve hely!`)
 
 async function getWeatherdinamic() {
@@ -360,7 +368,7 @@ if (check !==undefined || check1 !==undefined || check2 !==undefined) {
 const error3 = new Discord.MessageEmbed()
   .setColor('#ff2200')
   .setTitle("Hiba - RSS betöltése sikertelen")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ok:', `Nincs kijelölve RSS feed!`)
 
   chars = msg.content.length;
@@ -437,7 +445,7 @@ client.on('message', msg => {
   const embed = new Discord.MessageEmbed()
   .setColor('#0d09e3')
   .setTitle("NSFW GIF")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .setImage(hub.getRandomHub('real'))
   msg.reply(embed)
   }
@@ -674,7 +682,7 @@ async function getWeather() {
   .setTitle("Budapesti időjárás")
   .setColor('#0c9417')
   .setDescription("with weather-api-data (JS)")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Hőmérséklet', data.current.temp_c + 'C°')
   .addField('Idő', data.location.localtime)
   .addField('Időjárás',data.current.condition.text)
@@ -693,7 +701,7 @@ client.on('message', msg => {
   .setTitle("Mai névnap")
   .setColor('#0e58cf')
   .setDescription("Magyar névnapok.")
-  .setAuthor("Boobinator")
+  .setAuthor(`${name}`)
   .addField('Ma:', nevnap.today())
   msg.reply(embed)
 }
@@ -702,43 +710,171 @@ client.on('message', msg => {
 const pg1 = new Discord.MessageEmbed()
 .setTitle("Command List.")
 .setDescription("Parancsok listája. Prefixum: .")
-.setAuthor("Boobinator")
+.setAuthor(`${name}`)
 .addField("Moderáció", "A csetbot képes a csúnya szavak szűrésére, szövegkörnyezetben is.")
 .addField(".help", "Ezen útmutató lekérése")
 .addField(".porn", "Véletlenszerű NSFW GIF lekérése")
 .addField(".joke", "Véletlenszerű vicc az adatbázisból (bővítés alatt)")
 .addField(".covid", "Covid információk a világra és országunkra vonatkozóan.")
 .addField(".coin", "Egy pénz véletlen feldobása.")
+.addField(".kor", "Átlagéletkor lekérése név alapján.")
 
 const pg2 = new Discord.MessageEmbed()
 .setTitle("Command List.")
 .setDescription("Parancsok listája. Prefixum: .")
-.setAuthor("Boobinator")
+.setAuthor(`${name}`)
 .addField(".meme", "Egy Véletlenszerű Reddit mém lekérése.")
 .addField(".weather", "Budapesti időjárás lekérése")
 .addField(".weatherd <hely>", "Időjárás <hely>-re vonatkozóan.")
 .addField(".rss", "A telex.hu híroldal RSS-híreinek lekérése.")
 .addField(".rssd <page> <url>", "RSS hírek <url> helyről, a <page> oldalról.")
 .addField(".ban <member> ", "<member> kitiltása a szerverről. (Csak megfelelő ranggal használható.)")
+.addField('.billboard', 'Top 3 zene a Billboard Hot 100-ról.')
 
 
 const pg3 = new Discord.MessageEmbed()
 .setTitle("Command List.")
 .setDescription("Parancsok listája. Prefixum: .")
-.setAuthor("Boobinator")
+.setAuthor(`${name}`)
 .addField(".kick <member> ", "<member> kidobása a szerverről. (Csak megfelelő ranggal használható.)")
 .addField(".xp", "Az üzenet küldőjének XP-pontjainak lekérése a bot adatbázisából (1 üzenet, 1 pont)")
 .addField(".purge <count> ", "<count> számú üzenet törlése a csatornából. (Csak megfelelő ranggal használható.)")
 .addField('.minecraft' , 'Hypixel szerver státusz.')
 .addField('.minecraftd <url>' , 'Minecraft szerver státusz <url>-hez.')
 .addField('.nevnap', 'Mai névnap lekérése (Magyarország).')
-
+.addField('.arfolyam', 'USD-HUF és EUR-HUF árfolyam lekérése.')
 const pages = [
     pg1,
     pg2,
     pg3
 ];
 
+client.on('message', msg => {
+  if (msg.content === '.arfolyam') {
+    leveling.AddXp(msg.member.user.id, -1)
+
+async function arfolyam() {
+    const data = await currency.getCurrency("eur-huf")
+
+var xValues = ["Most",2.,3.,4.,5.,6.,7.];
+var yValues = [data.lastValue,data.transactions.slice(-2)[0].value,data.transactions.slice(-3)[0].value,data.transactions.slice(-4)[0].value,data.transactions.slice(-5)[0].value,data.transactions.slice(-6)[0].value,data.transactions.slice(-7)[0].value];
+
+const img1 = new ChartJsImage();
+img1.setConfig ({
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(38,128,10)",
+      borderColor: "rgb(16,69,0)",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 340, max:370}}],
+    }
+  }
+
+});
+
+    const embed = new Discord.MessageEmbed()
+  .setTitle(data.name)
+  .setColor('#069ecc')
+  .setDescription("Árfolyam.")
+  .setAuthor(`${name}`)
+  .addField(`Jelenlegi árfolyam:`,`${data.lastValue} HUF`)
+  .addField(`Régebbi értékek`,`${data.transactions.slice(-2)[0].value} HUF`)
+  .addField(`Régebbi értékek`,`${data.transactions.slice(-3)[0].value} HUF`)
+  .setImage(img1.getUrl())
+  msg.channel.send(embed)
+
+
+  const data1 = await currency.getCurrency("usd-huf")
+
+var xValues2 = ["Most",2.,3.,4.,5.,6.,7.];
+var yValues2 = [data1.lastValue,data1.transactions.slice(-2)[0].value,data1.transactions.slice(-3)[0].value,data1.transactions.slice(-4)[0].value,data1.transactions.slice(-5)[0].value,data1.transactions.slice(-6)[0].value,data1.transactions.slice(-7)[0].value];
+
+const img2 = new ChartJsImage();
+img2.setConfig ({
+  type: "line",
+  data: {
+    labels: xValues2,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgb(38,128,10)",
+      borderColor: "rgb(16,69,0)",
+      data: yValues2
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 280, max:320}}],
+    }
+  }
+  
+});
+
+  const embed1 = new Discord.MessageEmbed()
+  .setTitle(data1.name)
+  .setColor('#069ecc')
+  .setDescription("Árfolyam.")
+  .setAuthor(`${name}`)
+  .addField(`Jelenlegi árfolyam:`,`${data1.lastValue} HUF`)
+  .addField(`Régebbi értékek`,`${data1.transactions.slice(-2)[0].value} HUF`)
+  .addField(`Régebbi értékek`,`${data1.transactions.slice(-3)[0].value} HUF`)
+  .setImage(img2.getUrl())
+  msg.channel.send(embed1)
+  }
+arfolyam();
+}
+});
+
+client.on('message', msg => {
+  if (msg.content === '.billboard') {
+    leveling.AddXp(msg.member.user.id, -1)
+  billboardTop100.getChart((err, chart) => {
+  if (err) console.log(err);
+  const embed = new Discord.MessageEmbed()
+  .setTitle('Billboard zenelista')
+  .setColor('#fcba03')
+  .setDescription("Top 3 a Billboard Top 100-ról.")
+  .setAuthor(`${name}`)
+  .addField(chart.songs[0].title,chart.songs[0].artist)
+  .addField(chart.songs[1].title,chart.songs[1].artist)
+  .addField(chart.songs[2].title,chart.songs[2].artist)
+  .setImage('https://upload.wikimedia.org/wikipedia/commons/2/2b/Billboard_Hot_100_logo.jpg')
+  msg.channel.send(embed)
+});
+ 
+}
+});
+
+
+client.on('message', msg => {
+  if (msg.content.startsWith('.kor')) {
+    leveling.AddXp(msg.member.user.id, -1)
+  const name = msg.content.slice(4).trim()
+  request(`https://api.agify.io/?name=${name}`, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  const embed1 = new Discord.MessageEmbed()
+  .setTitle(res.body.name)
+  .setColor('#35d0e8')
+  .setDescription("Életkor név alapján.")
+  .setAuthor(`${name}`)
+  .addField(`Név:`,`${res.body.name}`)
+  .addField(`Átlagéletkor:`,`${res.body.age} év`)
+  .addField(`Ennyi adat alapján:`,`${res.body.count} db`)
+  msg.channel.send(embed1)
+});
+
+  }
+});
 
 
 
@@ -756,6 +892,6 @@ client.on('message', msg => {
 
 //Token helye
 
-client.login('TOKEN');
+client.login('ODYzMDcyNjY0ODYzNzY4NjA2.YOhlDg.NhNpTMJb0yO5mnKnXhXGF2BQn28');
 
 
